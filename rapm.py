1ea5904907d73a9d90ca22e3d1d7f93e8a8b7925
Jeremias Engelmann <jeremias.engelmann@gmail.com>
2024-06-05 11:00:47 +0700

    Update rapm.py
    Added weighing of observations

--- rapm.py
+++ rapm.py
@@ -4,7 +4,7 @@ from scipy.sparse import lil_matrix, csr_matrix
 
 def main():
 	#cur = MySQLdb.connect()
-	cur.execute("select home_poss, pts, a1, a2, a3, a4, a5, h1, h2, h3, h4, h5 from matchups")
+	cur.execute("select home_poss, pts, a1, a2, a3, a4, a5, h1, h2, h3, h4, h5, season from matchups")
 	data = cur.fetchall()
 
 	all_players = {}#get all players in the dataset
@@ -23,10 +23,13 @@ def main():
 
 	X = lil_matrix((len(data), len(col_to_player)))#use sparse matrixes so memory doesn't blow up
 	y = np.zeros(len(data))
+	sample_weights = []
+	season_weights = {2024: 1.0, 2023: 1.0, 2022: 1.0}#etc.
 	counter = 0
 	for item in data:
 		home_poss = item[0]
 		pts = item[1]
+		season = item[12]
 		home_list = []; away_list = []
 		for i in range(2, 7):
 			away_list.append(item[i])
@@ -43,12 +46,13 @@ def main():
 			def_p = str(p)+'_def'
 			X[counter, player_to_col[def_p]] = 1#'switch on' dummy variables for all defensive players present
 		y[counter] = pts
+		sample_weights.append(season_weights[season])
 		counter += 1
 
 	y -= np.average(y)
 	X = X.tocsr()#faster than lil_matrix for computation
 	clf = linear_model.Ridge(alpha = 3000)#Other options are RidgeCV ElasticNetCV, Lasso etc, all with different penalization parameters
-	clf.fit(X, y)
+	clf.fit(X, y, sample_weight = sample_weights)
 	for i in range(0, len(clf.coef_)):
 		print (col_to_player[i], ';', clf.coef_[i])
 
